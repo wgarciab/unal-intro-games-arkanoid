4831d415c11adc59acea1b1fd3b60e005349de0d
Wilmar Garcia <wgarciab@unal.edu.co>
2022-01-14 21:49:57 -0500

    Added part 5: BallVelocity

--- Assets/Scr/Ball.cs
+++ Assets/Scr/Ball.cs
@@ -3,8 +3,14 @@ using Random = UnityEngine.Random;
 
 public class Ball : MonoBehaviour
 {
+    private const float BALL_VELOCITY_MIN_AXIS_VALUE = 0.5f;
+    
     [SerializeField]
     private float _initSpeed = 5;
+    [SerializeField]
+    private float _minSpeed = 4;
+    [SerializeField]
+    private float _maxSpeed = 7;
 
     private Rigidbody2D _rb;
     private Collider2D _collider;
@@ -21,5 +27,36 @@ public class Ball : MonoBehaviour
         
         _collider.enabled = true;
         _rb.velocity = Random.insideUnitCircle.normalized * _initSpeed;
+    }
+    
+    void FixedUpdate()
+    {
+        CheckVelocity();
+    }
+    
+    private void CheckVelocity()
+    {
+        Vector2 velocity = _rb.velocity;
+        float currentSpeed = velocity.magnitude;
+        
+        if (currentSpeed < _minSpeed)
+        {
+            velocity = velocity.normalized * _minSpeed;
+        }
+        else if (currentSpeed > _maxSpeed)
+        {
+            velocity = velocity.normalized * _maxSpeed;
+        }
+        
+        if(Mathf.Abs(velocity.x) < BALL_VELOCITY_MIN_AXIS_VALUE) 
+        {
+            velocity.x += Mathf.Sign(velocity.x) * BALL_VELOCITY_MIN_AXIS_VALUE * Time.deltaTime;
+        }
+        else if (Mathf.Abs(velocity.y) < BALL_VELOCITY_MIN_AXIS_VALUE)
+        {
+            velocity.y += Mathf.Sign(velocity.y) * BALL_VELOCITY_MIN_AXIS_VALUE * Time.deltaTime;
+        }
+
+        _rb.velocity = velocity;
     }
 }
